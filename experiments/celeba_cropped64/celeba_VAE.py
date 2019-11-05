72b9efeadcd22feda8feccea8f6251d1fa4bae85
gerben <gerry2211@gmail.com>
2019-11-05 11:00:28 +0100

    Added a 64x64 CelebA VAE experiment

---
+++ experiments/celeba_cropped64/celeba_VAE.py
@@ -0,0 +1,68 @@
+from models.conv64.encoder import Encoder64
+from models.conv64.generator import Generator64
+from trainloops.listeners.model_saver import ModelSaver
+from trainloops.vae_train_loop import VaeTrainLoop
+from data.celeba_cropped import CelebaCropped
+import util.output
+from torchvision import transforms
+import torch
+import argparse
+
+# Parse commandline arguments
+from trainloops.listeners.gan_image_sample_logger import GanImageSampleLogger
+from trainloops.listeners.loss_reporter import LossReporter
+
+parser = argparse.ArgumentParser(description="Celeba VAE experiment.")
+parser.add_argument("--batch_size", action="store", type=int, default=64, help="Changes the batch size, default is 64")
+parser.add_argument("--lr", action="store", type=float, default=0.0001,
+                    help="Changes the learning rate, default is 0.0001")
+parser.add_argument("--h_size", action="store", type=int, default=16,
+                    help="Sets the h_size, which changes the size of the network")
+parser.add_argument("--epochs", action="store", type=int, default=100, help="Sets the number of training epochs")
+parser.add_argument("--d_steps", action="store", type=int, default=2,
+                    help="Amount of discriminator steps per generator step")
+parser.add_argument("--l_size", action="store", type=int, default=12, help="Size of the latent space")
+parser.add_argument("--cuda", action="store_true", default=False,
+                    help="Enables CUDA support. The script will fail if cuda is not available")
+parser.add_argument("--use_mish", action="store_true", default=False,
+                    help="Changes all activations except the ouput of D and G to mish, which might work better")
+parser.add_argument("--no_bias_in_dec", action="store_true", default=False, help="Disables biases in the decoder")
+# parser.add_argument("--load_path", action="store", type=str, default=None,
+#                     help="When given, loads models from LOAD_PATH folder")
+# parser.add_argument("--save_path", action="store", type=str, default=None,
+#                     help="When given, saves models to LOAD_PATH folder after all epochs (or every epoch)")
+# parser.add_argument("--save_every_epoch", action="store_true", default=False,
+#                     help="When a save path is given, store the model after every epoch instead of only the last")
+# parser.add_argument("--img_path", action="store", type=str, default=None,
+#                     help="When given, saves samples to the given directory")
+
+args = parser.parse_args()
+
+output_path = util.output.init_experiment_output_dir("celeba64", "vae", args)
+
+dataset = CelebaCropped(split="train", download=True, transform=transforms.Compose([
+    transforms.Resize(64),
+    transforms.ToTensor(),
+    transforms.Lambda(lambda img: img * 2 - 1)
+]))
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=12)
+
+
+enc = Encoder64(args.l_size, args.h_size, args.use_mish, n_channels=3)
+dec = Generator64(args.l_size, args.h_size, args.use_mish, not args.no_bias_in_dec, n_channels=3)
+enc_optimizer = torch.optim.Adam(enc.parameters(), lr=args.lr, betas=(0.5, 0.999))
+dec_optimizer = torch.optim.Adam(dec.parameters(), lr=args.lr, betas=(0.5, 0.999))
+
+if args.cuda:
+    enc = enc.cuda()
+    dec = dec.cuda()
+
+listeners = [
+    LossReporter(),
+    GanImageSampleLogger(output_path, args, pad_value=1),
+    ModelSaver(output_path, n=5, overwrite=True, print_output=True)
+]
+train_loop = VaeTrainLoop(listeners, enc, dec, enc_optimizer, dec_optimizer, dataloader,
+                          cuda=args.cuda, epochs=args.epochs)
+
+train_loop.train()
---
+++ models/conv64/encoder.py
@@ -0,0 +1,66 @@
+import torch
+
+from models.morphing_encoder import MorphingEncoder
+from util.torch.activations import mish
+
+
+class Encoder64(MorphingEncoder):
+    def __init__(self, latent_size, h_size, use_mish=False, n_channels=1):
+        super().__init__()
+
+        self.n_channels = n_channels
+
+        if use_mish:
+            self.activ = mish
+        else:
+            self.activ = torch.nn.functional.relu
+
+        self.latent_size = latent_size
+        self.h_size = h_size
+        self.conv_1 = torch.nn.Conv2d(n_channels, h_size, kernel_size=3,  stride=1)
+        self.conv_2 = torch.nn.Conv2d(h_size, h_size * 2, kernel_size=4, stride=2)
+        self.conv_3 = torch.nn.Conv2d(h_size * 2, h_size * 2, kernel_size=4, stride=2)
+        self.conv_4 = torch.nn.Conv2d(h_size * 2, h_size * 4, kernel_size=8, stride=2)
+        self.conv_5 = torch.nn.Conv2d(h_size * 4, h_size * 4, kernel_size=4, stride=1)
+
+
+        self.bn_2 = torch.nn.BatchNorm2d(self.h_size * 2)
+        self.bn_3 = torch.nn.BatchNorm2d(self.h_size * 2)
+        self.bn_4 = torch.nn.BatchNorm2d(self.h_size * 4)
+        self.bn_5 = torch.nn.BatchNorm2d(self.h_size * 4)
+
+        self.mean_fc = torch.nn.Linear(h_size * 4, latent_size)
+        self.std_fc = torch.nn.Linear(h_size * 4, latent_size)
+
+    def forward(self, inp):
+        x = self.conv_1(inp)
+        x = self.activ(x)
+
+        x = self.conv_2(x)
+        x = self.bn_2(x)
+        x = self.activ(x)
+
+        x = self.conv_3(x)
+        x = self.bn_3(x)
+        x = self.activ(x)
+
+        x = self.conv_4(x)
+        x = self.bn_4(x)
+        x = self.activ(x)
+
+        x = self.conv_5(x)
+        x = self.bn_5(x)
+        x = self.activ(x)
+
+        # Flatten to vector
+        x = x.view(-1, self.h_size * 4)
+
+        means = self.mean_fc(x)
+        log_vars = self.std_fc(x)
+        return self.sample(means, log_vars), means, log_vars
+
+    @staticmethod
+    def sample(means, vars):
+        stds = torch.exp(0.5 * vars)
+        eps = torch.randn_like(stds)
+        return means + eps * stds
