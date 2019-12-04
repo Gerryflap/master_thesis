Completely reworked the ALI implementation. It is now "single step" again, D and G are updated on the same images and on the same computation graph. Also fixed some problems in the Encoders with respect to BatchNorm and exploding variance. Also cleaned up code in some places

experiments/celeba_cropped28/celeba_MorGAN.py
@@ -1,5 +1,5 @@
 from models.conv28.encoder import Encoder28
-from trainloops.ali_train_loop_single_step import ALITrainLoop
+from trainloops.ali_train_loop import ALITrainLoop
 from trainloops.gan_train_loop import GanTrainLoop
 from models.conv28.ali_discriminator import ALIDiscriminator28
 from models.conv28.generator import Generator28
@@ -62,7 +62,7 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, sh
 
 print("Dataset length: ", len(dataset))
 
-Gz = Encoder28(args.l_size, args.h_size, args.use_mish, n_channels=3)
+Gz = Encoder28(args.l_size, args.h_size, args.use_mish, n_channels=3, cap_variance=True)
 Gx = Generator28(args.l_size, args.h_size, args.use_mish, n_channels=3, sigmoid_out=True)
 D = ALIDiscriminator28(args.l_size, args.h_size, use_bn=args.use_batchnorm_in_D, use_mish=args.use_mish, n_channels=3, dropout=args.dropout_rate, fc_h_size=args.fc_h_size)
 G_optimizer = torch.optim.Adam(list(Gz.parameters()) + list(Gx.parameters()), lr=args.lr, betas=(0.5, 0.999))
@@ -82,10 +82,7 @@ listeners = [
     AEImageSampleLogger(output_path, valid_dataset, args, folder_name="AE_samples_valid", print_stats=True),
     AEImageSampleLogger(output_path, dataset, args, folder_name="AE_samples_train"),
     # DiscriminatorOverfitMonitor(dataset, valid_dataset, 100, args),
-    ParameterValueLogger(output_path, "Conv"),
-    ParameterValueLogger(output_path, "Norm"),
     ModelSaver(output_path, n=1, overwrite=True, print_output=True),
-    ModelSaver(output_path, n=4, overwrite=False, print_output=True),
 ]
 train_loop = ALITrainLoop(
     listeners=listeners,
@@ -99,7 +96,8 @@ train_loop = ALITrainLoop(
     epochs=args.epochs,
     morgan_alpha=args.morgan_alpha,
     d_real_label=args.d_real_label,
-    d_img_noise_std=args.instance_noise_std
+    d_img_noise_std=args.instance_noise_std,
+    decrease_noise=True
 )
 
 train_loop.train()

experiments/celeba_cropped64/celeba_MorGAN.py
@@ -64,7 +64,7 @@ valid_dataset = CelebaCropped(split="valid", download=True, morgan_like_filterin
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
 
 if args.continue_with is None:
-    Gz = Encoder64(args.l_size, args.h_size, args.use_mish, n_channels=3)
+    Gz = Encoder64(args.l_size, args.h_size, args.use_mish, n_channels=3, cap_variance=True)
     Gx = Generator64(args.l_size, args.h_size, args.use_mish, n_channels=3, sigmoid_out=True)
     D = ALIDiscriminator64(args.l_size, args.h_size, use_bn=not args.disable_batchnorm_in_D, use_mish=args.use_mish,
                            n_channels=3, dropout=args.dropout_rate, fc_h_size=args.fc_h_size)
@@ -107,7 +107,8 @@ train_loop = ALITrainLoop(
     epochs=args.epochs,
     morgan_alpha=args.morgan_alpha,
     d_real_label=args.d_real_label,
-    d_img_noise_std=args.instance_noise_std
+    d_img_noise_std=args.instance_noise_std,
+    decrease_noise=True
 
 )
 

experiments/mnist/mnist_GAN.py
@@ -6,12 +6,10 @@ import util.output
 from torchvision import transforms
 import torch
 import argparse
-
-# Parse commandline arguments
 from trainloops.listeners.gan_image_sample_logger import GanImageSampleLogger
 from trainloops.listeners.loss_reporter import LossReporter
-from util.torch.initialization import weights_init
 
+# Parse commandline arguments
 parser = argparse.ArgumentParser(description="MNIST DCGAN experiment.")
 parser.add_argument("--batch_size", action="store", type=int, default=64, help="Changes the batch size, default is 64")
 parser.add_argument("--lr", action="store", type=float, default=0.0001,

models/conv28/encoder.py
@@ -6,11 +6,12 @@ from util.torch.initialization import weights_init
 
 
 class Encoder28(MorphingEncoder):
-    def __init__(self, latent_size, h_size, use_mish=False, n_channels=1, deterministic=False):
+    def __init__(self, latent_size, h_size, use_mish=False, n_channels=1, deterministic=False, cap_variance=True):
         super().__init__()
 
         self.n_channels = n_channels
         self.deterministic = deterministic
+        self.cap_variance = cap_variance
 
         if use_mish:
             self.activ = mish
@@ -64,6 +65,8 @@ class Encoder28(MorphingEncoder):
             return means, means, torch.ones_like(means)*-30
         else:
             log_vars = self.std_fc(x)
+            if self.cap_variance:
+                log_vars = -torch.nn.functional.softplus(log_vars)
             return self.sample(means, log_vars), means, log_vars
 
     @staticmethod

models/conv64_ali/encoder.py
@@ -13,11 +13,12 @@ from util.torch.initialization import weights_init
 
 
 class Encoder64(MorphingEncoder):
-    def __init__(self, latent_size=512, h_size=64, use_mish=False, n_channels=3, deterministic=False):
+    def __init__(self, latent_size=512, h_size=64, use_mish=False, n_channels=3, deterministic=False, cap_variance=True):
         super().__init__()
 
         self.n_channels = n_channels
         self.deterministic = deterministic
+        self.cap_variance = cap_variance
 
         if use_mish:
             self.activ = mish
@@ -33,6 +34,7 @@ class Encoder64(MorphingEncoder):
         self.conv_4 = torch.nn.Conv2d(h_size * 4, h_size * 4, kernel_size=7, stride=2, bias=False)
         self.conv_5 = torch.nn.Conv2d(h_size * 4, h_size * 8, kernel_size=4, stride=1, bias=False)
 
+        self.bn_1 = torch.nn.BatchNorm2d(self.h_size)
         self.bn_2 = torch.nn.BatchNorm2d(self.h_size * 2)
         self.bn_3 = torch.nn.BatchNorm2d(self.h_size * 4)
         self.bn_4 = torch.nn.BatchNorm2d(self.h_size * 4)
@@ -50,6 +52,7 @@ class Encoder64(MorphingEncoder):
 
     def forward(self, inp):
         x = self.conv_1(inp)
+        x = self.bn_1(x)
         x = self.activ(x)
 
         x = self.conv_2(x)
@@ -79,6 +82,8 @@ class Encoder64(MorphingEncoder):
             return means, means, torch.ones_like(means)*-30.0
         else:
             log_vars = self.std_fc(x)
+            if self.cap_variance:
+                log_vars = -torch.nn.functional.softplus(log_vars)
             return self.sample(means, log_vars), means, log_vars
 
     @staticmethod

trainloops/ali_train_loop.py
@@ -9,7 +9,7 @@ from trainloops.train_loop import TrainLoop
 
 
 class ALITrainLoop(TrainLoop):
-    def __init__(self, listeners: list, Gz, Gx, D, optim_G, optim_D, dataloader, cuda=False, epochs=1, morgan_alpha=0.0, d_img_noise_std=0.0, d_real_label=1.0):
+    def __init__(self, listeners: list, Gz, Gx, D, optim_G, optim_D, dataloader, cuda=False, epochs=1, morgan_alpha=0.0, d_img_noise_std=0.0, d_real_label=1.0, decrease_noise=True):
         super().__init__(listeners, epochs)
         self.batch_size = dataloader.batch_size
         self.Gz = Gz
@@ -24,6 +24,7 @@ class ALITrainLoop(TrainLoop):
         self.morgan = morgan_alpha != 0
         self.d_img_noise_std = d_img_noise_std
         self.d_real_label = d_real_label
+        self.decrease_noise = decrease_noise
 
     def epoch(self):
         self.Gx.train()
@@ -38,76 +39,60 @@ class ALITrainLoop(TrainLoop):
             # Draw M (= batch_size) samples from dataset and prior. x samples are already loaded by dataloader
             if self.cuda:
                 x = x.cuda()
-            z = self.generate_z_batch(self.batch_size)
 
-            self.optim_D.zero_grad()
+            # ========== Computations for Dis(x, z_hat) ==========
 
-            # Sample from conditionals (sampling is implemented by models)
-            z_hat = self.Gz.encode(x)
-            xr_d_inp = x
-
-            # Add noise to the inputs of D if the standard deviation isn't defined to be 0
+            x_no_noise = x
+            # Add noise to the inputs if the standard deviation isn't defined to be 0
             if self.d_img_noise_std != 0.0:
-                xr_d_inp += torch.randn_like(xr_d_inp)*self.d_img_noise_std
+                x = self.add_instance_noise(x)
 
-            dis_q = self.D((xr_d_inp, z_hat.detach()))
-            d_real_labels = torch.ones_like(dis_q)
-            if self.d_real_label != 1.0:
-                d_real_labels *= self.d_real_label
-            L_d_real = F.binary_cross_entropy_with_logits(dis_q, d_real_labels, reduction="mean")
-            L_d_real.backward()
-
-            x_tilde = self.Gx(z)
-            xf_d_inp = x_tilde.detach()
-
-            # Add noise to the inputs of D if the standard deviation isn't defined to be 0
-            if self.d_img_noise_std != 0.0:
-                xf_d_inp += torch.randn_like(xf_d_inp)*self.d_img_noise_std
-
-            dis_p = self.D((xf_d_inp, z))
-            L_d_fake = F.binary_cross_entropy_with_logits(dis_p, torch.zeros_like(dis_q), reduction="mean")
-            L_d_fake.backward()
-
-            L_d = L_d_real + L_d_fake
-
-            # Gradient update on Discriminator network
-            torch.nn.utils.clip_grad_norm_(list(self.D.parameters()), 1.0)
-            self.optim_D.step()
-
-            # Train G
-            self.optim_G.zero_grad()
-
-            # Compute and backpropagate loss for x_tilde
-            z = self.generate_z_batch(self.batch_size)
-            x_tilde = self.Gx(z)
-            dis_p = self.D((x_tilde, z))
-            L_gx = F.binary_cross_entropy_with_logits(dis_p, torch.ones_like(dis_q), reduction="mean")
-            L_gx.backward()
-
-            # Compute and backpropagate loss for z_hat
             # Sample from conditionals (sampling is implemented by models)
             z_hat = self.Gz.encode(x)
             dis_q = self.D((x, z_hat))
-            L_gz = F.binary_cross_entropy_with_logits(dis_q, torch.zeros_like(dis_q), reduction="mean")
-            # Keep the graph if we're using MorGAN, since we still need it to backprop through Gz
-            L_gz.backward(retain_graph=self.morgan)
 
-            L_g = L_gz + L_gx
+            # ========== Computations for Dis(x_tilde, z) ==========
 
-            # Extra code for the MorGAN algorithm. This is not part of ALI
+            z = self.generate_z_batch(self.batch_size)
+            x_tilde = self.Gx(z)
+            # Add noise to the inputs of D if the standard deviation isn't defined to be 0
+            if self.d_img_noise_std != 0.0:
+                x_tilde = self.add_instance_noise(x_tilde)
+
+            dis_p = self.D((x_tilde, z))
+
+            # ========== Loss computations ==========
+
+            L_d_fake = F.binary_cross_entropy_with_logits(dis_p, torch.zeros_like(dis_q))
+            d_real_labels = torch.ones_like(dis_q) * self.d_real_label
+            L_d_real = F.binary_cross_entropy_with_logits(dis_q, d_real_labels)
+            L_d = L_d_real + L_d_fake
+
+            L_g_fake = F.binary_cross_entropy_with_logits(dis_p, torch.ones_like(dis_q))
+            L_g_real = F.binary_cross_entropy_with_logits(dis_q, torch.zeros_like(dis_q))
+
+            L_g = L_g_real + L_g_fake
+
             if self.morgan:
                 x_recon = self.Gx(z_hat)
-                L_pixel = self.morgan_alpha * self.morgan_pixel_loss(x_recon, x)
-                L_syn = L_g + L_pixel
+                L_pixel = self.morgan_pixel_loss(x_recon, x_no_noise)
+                L_syn = L_g + self.morgan_alpha * L_pixel
 
-            # Gradient update on Generator networks
+            # ========== Back propagation and updates ==========
+
+            # Gradient update on Discriminator network
+            if L_g.detach().item() < 3.5:
+                self.optim_D.zero_grad()
+                L_d.backward(retain_graph=True)
+                self.optim_D.step()
+
+            # Gradient update on the Generator networks
+            self.optim_G.zero_grad()
             if self.morgan:
-                L_pixel.backward()
-            torch.nn.utils.clip_grad_norm_(list(self.Gx.parameters()) + list(self.Gz.parameters()), 1.0)
+                L_syn.backward()
+            else:
+                L_g.backward()
             self.optim_G.step()
-        self.Gx.eval()
-        self.Gz.eval()
-        self.D.eval()
 
         losses = {
                 "D_loss": L_d.detach().item(),
@@ -152,3 +137,8 @@ class ALITrainLoop(TrainLoop):
         # loss = absolute_errors.sum()/WxH
         loss = absolute_errors.mean()
         return loss
+
+    def add_instance_noise(self, x):
+        noise_factor = self.d_img_noise_std * \
+                       (1 if not self.decrease_noise else 1 - (self.current_epoch / self.epochs))
+        return x + torch.randn_like(x) * noise_factor