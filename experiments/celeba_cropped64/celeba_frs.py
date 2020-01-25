e6ba4f3a67a05b48895abdc55684ab2bd624931f
gerryflap <gerry2211@gmail.com>
2020-01-25 19:44:24 +0100

    Added celeba_frs for 64x64

---
+++ experiments/celeba_cropped64/celeba_frs.py
@@ -0,0 +1,69 @@
+"""
+    Trains a facial recognition system for celeba_cropped28.
+"""
+import argparse
+
+import torch
+from torchvision import transforms
+
+import util.output
+from data.celeba_cropped_triplets import CelebaCroppedTriplets
+from models.conv64.frs import FRS64
+from trainloops.frs_trainloop import FRSTrainLoop
+from trainloops.listeners.cluster_killswitch import KillSwitchListener
+from trainloops.listeners.frs_accuracy_printer import FRSAccuracyPrinter
+from trainloops.listeners.loss_reporter import LossReporter
+from trainloops.listeners.model_saver import ModelSaver
+
+parser = argparse.ArgumentParser(description="Celeba FRS experiment.")
+parser.add_argument("--batch_size", action="store", type=int, default=65, help="Changes the batch size, default is 65")
+parser.add_argument("--lr", action="store", type=float, default=0.0001,
+                    help="Changes the learning rate, default is 0.0001")
+parser.add_argument("--h_size", action="store", type=int, default=64,
+                    help="Sets the h_size, which changes the size of the network")
+parser.add_argument("--epochs", action="store", type=int, default=100, help="Sets the number of training epochs")
+parser.add_argument("--l_size", action="store", type=int, default=128, help="Size of the latent space")
+parser.add_argument("--cuda", action="store_true", default=False,
+                    help="Enables CUDA support. The script will fail if cuda is not available")
+parser.add_argument("--use_mish", action="store_true", default=False,
+                    help="Changes all activations except the ouput of D and G to mish, which might work better")
+
+args = parser.parse_args()
+
+output_path = util.output.init_experiment_output_dir("celeba64", "frs", args)
+
+dataset = CelebaCroppedTriplets(split="train", download=True, transform=transforms.Compose([
+    transforms.ToTensor(),
+]))
+
+valid_dataset = CelebaCroppedTriplets(split="valid", download=True, transform=transforms.Compose([
+    transforms.ToTensor(),
+]))
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+frs_model = FRS64(args.l_size, args.h_size, use_mish=args.use_mish, n_channels=3)
+
+if args.cuda:
+    frs_model = frs_model.cuda()
+
+optimizer = torch.optim.Adam(frs_model.parameters(), lr=args.lr)
+
+frs_model.init_weights()
+
+listeners = [
+    LossReporter(),
+    ModelSaver(output_path, n=1, overwrite=True, print_output=True),
+    FRSAccuracyPrinter(args.cuda, valid_dataset),
+    KillSwitchListener(output_path)
+]
+
+trainloop = FRSTrainLoop(
+    listeners,
+    frs_model,
+    optimizer,
+    dataloader,
+    cuda=args.cuda,
+    epochs=args.epochs
+)
+trainloop.train()
---
+++ models/conv64/frs.py
@@ -0,0 +1,76 @@
+"""
+    Aims to reproduce the Encoder from the ALI paper.
+
+    The deterministic mode has been added, but is not part of ALI.
+    In this mode the encoder predicts the z directly instead of returning a distribution.
+"""
+
+import torch
+
+from models.morphing_encoder import MorphingEncoder
+from util.torch.activations import mish, map_to_hypersphere
+from util.torch.initialization import weights_init
+
+
+class FRS64(torch.nn.Module):
+    def __init__(self, latent_size=512, h_size=64, use_mish=False, n_channels=3):
+        super().__init__()
+
+        self.n_channels = n_channels
+
+        if use_mish:
+            self.activ = mish
+        else:
+            self.activ = self.leaky_relu
+
+        self.latent_size = latent_size
+        self.h_size = h_size
+
+        self.conv_1 = torch.nn.Conv2d(n_channels, h_size, kernel_size=2, stride=1, bias=False)
+        self.conv_2 = torch.nn.Conv2d(h_size, h_size * 2, kernel_size=7, stride=2, bias=False)
+        self.conv_3 = torch.nn.Conv2d(h_size * 2, h_size * 4, kernel_size=5, stride=2, bias=False)
+        self.conv_4 = torch.nn.Conv2d(h_size * 4, h_size * 4, kernel_size=7, stride=2, bias=False)
+        self.conv_5 = torch.nn.Conv2d(h_size * 4, h_size * 8, kernel_size=4, stride=1, bias=False)
+
+        self.bn_1 = torch.nn.BatchNorm2d(self.h_size)
+        self.bn_2 = torch.nn.BatchNorm2d(self.h_size * 2)
+        self.bn_3 = torch.nn.BatchNorm2d(self.h_size * 4)
+        self.bn_4 = torch.nn.BatchNorm2d(self.h_size * 4)
+        self.bn_5 = torch.nn.BatchNorm2d(self.h_size * 8)
+
+        self.out_fc = torch.nn.Linear(h_size * 8, latent_size, bias=True)
+
+    @staticmethod
+    def leaky_relu(x):
+        return torch.nn.functional.leaky_relu(x, 0.02)
+
+    def forward(self, inp):
+        x = self.conv_1(inp)
+        x = self.bn_1(x)
+        x = self.activ(x)
+
+        x = self.conv_2(x)
+        x = self.bn_2(x)
+        x = self.activ(x)
+
+        x = self.conv_3(x)
+        x = self.bn_3(x)
+        x = self.activ(x)
+
+        x = self.conv_4(x)
+        x = self.bn_4(x)
+        x = self.activ(x)
+
+        x = self.conv_5(x)
+        x = self.bn_5(x)
+        x = self.activ(x)
+
+        # Flatten to vector
+        x = x.view(-1, self.h_size * 8)
+
+        out = self.out_fc(x)
+        out = map_to_hypersphere(out)
+        return out
+
+    def init_weights(self):
+        self.apply(weights_init)
