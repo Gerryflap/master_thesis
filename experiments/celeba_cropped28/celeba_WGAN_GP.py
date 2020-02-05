3a822cbc9df1095ef2f96ec9dbd035fb9231f100
gerryflap <gerry2211@gmail.com>
2020-02-05 16:13:25 +0100

    Added WGAN-GP

---
+++ experiments/celeba_cropped28/celeba_WGAN_GP.py
@@ -0,0 +1,61 @@
+from trainloops.gan_train_loop import GanTrainLoop
+from models.conv28.discriminator import Discriminator28
+from models.conv28.generator import Generator28
+from data.celeba_cropped import CelebaCropped
+import util.output
+from torchvision import transforms
+import torch
+import argparse
+
+# Parse commandline arguments
+from trainloops.listeners.gan_image_sample_logger import GanImageSampleLogger
+from trainloops.listeners.loss_reporter import LossReporter
+from trainloops.listeners.model_saver import ModelSaver
+
+parser = argparse.ArgumentParser(description="Celeba WGAN-GP experiment.")
+parser.add_argument("--batch_size", action="store", type=int, default=64, help="Changes the batch size, default is 64")
+parser.add_argument("--lr", action="store", type=float, default=0.0001,
+                    help="Changes the learning rate, default is 0.0001")
+parser.add_argument("--h_size", action="store", type=int, default=16,
+                    help="Sets the h_size, which changes the size of the network")
+parser.add_argument("--epochs", action="store", type=int, default=100, help="Sets the number of training epochs")
+parser.add_argument("--d_steps", action="store", type=int, default=2,
+                    help="Amount of discriminator steps per generator step")
+parser.add_argument("--l_size", action="store", type=int, default=12, help="Size of the latent space")
+parser.add_argument("--cuda", action="store_true", default=False,
+                    help="Enables CUDA support. The script will fail if cuda is not available")
+parser.add_argument("--use_mish", action="store_true", default=False,
+                    help="Changes all activations except the ouput of D and G to mish, which might work better")
+parser.add_argument("--dropout_rate", action="store", default=0.0, type=float,
+                    help="Sets the dropout rate in D")
+
+args = parser.parse_args()
+
+output_path = util.output.init_experiment_output_dir("celeba28", "wgan_gp", args)
+
+dataset = CelebaCropped(split="train", download=True, transform=transforms.Compose([
+    transforms.Resize(28),
+    transforms.ToTensor(),
+]))
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=12)
+
+G = Generator28(args.l_size, args.h_size, args.use_mish, bias=False, n_channels=3, sigmoid_out=True)
+D = Discriminator28(args.h_size, use_bn=False, use_mish=args.use_mish, n_channels=3, dropout=args.dropout_rate)
+G_optimizer = torch.optim.Adam(G.parameters(), lr=args.lr, betas=(0.5, 0.999))
+D_optimizer = torch.optim.Adam(D.parameters(), lr=args.lr, betas=(0.5, 0.999))
+
+if args.cuda:
+    G = G.cuda()
+    D = D.cuda()
+
+D.init_weights()
+
+listeners = [
+    LossReporter(),
+    GanImageSampleLogger(output_path, args, pad_value=1),
+    ModelSaver(output_path, n=5, overwrite=True, print_output=True)
+]
+train_loop = GanTrainLoop(listeners, G, D, G_optimizer, D_optimizer, dataloader, D_steps_per_G_step=args.d_steps,
+                          cuda=args.cuda, epochs=args.epochs)
+
+train_loop.train()
---
+++ trainloops/wgangp_train_loop.py
@@ -0,0 +1,108 @@
+from trainloops.train_loop import TrainLoop
+import torch
+import torch.nn.functional as F
+
+class GanTrainLoop(TrainLoop):
+    def __init__(
+            self,
+            listeners: list,
+            G,
+            D,
+            G_optimizer,
+            D_optimizer,
+            dataloader: torch.utils.data.DataLoader,
+            D_steps_per_G_step=2,
+            cuda=False,
+            epochs=1
+    ):
+        super().__init__(listeners, epochs)
+        self.batch_size = dataloader.batch_size
+        self.G = G
+        self.D = D
+        self.G_optimizer = G_optimizer
+        self.D_optimizer = D_optimizer
+        self.dataloader = dataloader
+        self.D_steps_per_G_step = D_steps_per_G_step
+        self.cuda = cuda
+
+    def epoch(self):
+
+        for i, (real_batch, _) in enumerate(self.dataloader):
+            if real_batch.size()[0] != self.batch_size:
+                continue
+            # Train D
+
+            # Make gradients for D zero
+            self.D.zero_grad()
+            if self.cuda:
+                real_batch = real_batch.cuda()
+
+            # Compute outputs for real images
+            d_real_outputs = self.D(real_batch)
+
+            # Generate a fake image batch
+            fake_batch = self.generate_batch(self.batch_size).detach()
+
+            # Compute outputs for fake images
+            d_fake_outputs = self.D(fake_batch)
+
+            # Compute losses
+            d_loss = (d_fake_outputs - d_real_outputs).mean()
+
+            eps = torch.randn((self.batch_size, 1, 1, 1))
+            if self.cuda:
+                eps = eps.cuda()
+            x_hat = eps * real_batch + (1.0-eps) * fake_batch
+            grad = torch.autograd.grad(self.D(x_hat).sum(), x_hat, create_graph=True, only_inputs=True)[0]
+            d_grad_loss = (torch.pow(grad, 2) - 1).mean()
+
+            d_loss = d_loss + 10.0 * d_grad_loss
+
+            # Update weights
+            self.D_optimizer.step()
+
+            if i % self.D_steps_per_G_step == 0:
+                # Train G (this is sometimes skipped to balance G and D according to the d_steps parameter)
+
+                # Make gradients for G zero
+                self.G.zero_grad()
+
+                # Generate a batch of fakes
+                fake_batch = self.generate_batch(self.batch_size)
+
+                # Compute loss for G, images should become more 'real' to the discriminator
+                g_loss = -self.D(fake_batch).mean()
+                g_loss.backward()
+
+                self.G_optimizer.step()
+
+
+
+        return {
+            "epoch": self.current_epoch,
+            "losses": {
+                "D_loss": d_loss.detach().item(),
+                "G_loss": g_loss.detach().item(),
+            },
+            "networks": {
+                "G": self.G,
+                "D": self.D,
+            },
+            "optimizers": {
+                "G_optimizer": self.G_optimizer,
+                "D_optimizer": self.D_optimizer
+            }
+        }
+
+    def generate_z_batch(self, batch_size):
+        z = torch.normal(torch.zeros((batch_size, self.G.latent_size)), 1)
+        if self.cuda:
+            z = z.cuda()
+        return z
+
+    def generate_batch(self, batch_size):
+        # Generate random latent vectors
+        z = self.generate_z_batch(batch_size)
+
+        # Return outputs
+        return self.G(z)
