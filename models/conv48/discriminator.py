90dfe2b5d94fc4a0c48706954309b9fce1cbe536
gerben <gerry2211@gmail.com>
2020-02-12 18:05:20 +0100

    Added more WGAN-GP experiments

---
+++ models/conv48/discriminator.py
@@ -0,0 +1,84 @@
+import torch
+from util.torch.activations import mish
+from util.torch.initialization import weights_init
+
+
+class Discriminator48(torch.nn.Module):
+    def __init__(self, h_size, use_bn=False, use_mish=False, n_channels=1, dropout=0.0, use_logits=True):
+        super().__init__()
+
+        self.use_logits = use_logits
+        self.n_channels = n_channels
+
+        if use_mish:
+            self.activ = mish
+        else:
+            self.activ = self.leaky_relu
+
+        self.dropout = dropout
+        self.h_size = h_size
+        self.conv_1 = torch.nn.Conv2d(n_channels, h_size, kernel_size=3,  stride=1)
+        self.conv_2 = torch.nn.Conv2d(h_size, h_size * 2, kernel_size=4, stride=2)
+        self.conv_3 = torch.nn.Conv2d(h_size * 2, h_size * 2, kernel_size=4, stride=2)
+        self.conv_4 = torch.nn.Conv2d(h_size * 2, h_size * 4, kernel_size=4, stride=2)
+        self.conv_5 = torch.nn.Conv2d(h_size * 4, h_size * 4, kernel_size=4, stride=1)
+
+        self.use_bn = use_bn
+        if use_bn:
+            self.bn_2 = torch.nn.BatchNorm2d(self.h_size * 2)
+            self.bn_3 = torch.nn.BatchNorm2d(self.h_size * 2)
+            self.bn_4 = torch.nn.BatchNorm2d(self.h_size * 4)
+            self.bn_5 = torch.nn.BatchNorm2d(self.h_size * 4)
+
+        if dropout != 0:
+            self.dropout_layer = torch.nn.Dropout(dropout, True)
+
+        self.lin_1 = torch.nn.Linear(h_size * 4, 1)
+
+
+
+    @staticmethod
+    def leaky_relu(x):
+        return torch.nn.functional.leaky_relu(x, 0.02)
+
+    def forward(self, inp):
+        x = self.conv_1(inp)
+        x = self.activ(x)
+
+        x = self.conv_2(x)
+        if self.use_bn:
+            x = self.bn_2(x)
+        x = self.activ(x)
+
+        x = self.conv_3(x)
+        if self.use_bn:
+            x = self.bn_3(x)
+        x = self.activ(x)
+
+        x = self.conv_4(x)
+        if self.use_bn:
+            x = self.bn_4(x)
+        x = self.activ(x)
+
+        x = self.conv_5(x)
+        if self.use_bn:
+            x = self.bn_5(x)
+        x = self.activ(x)
+
+        # Flatten to vector
+        x = x.view(-1, self.h_size * 4)
+
+        if self.dropout != 0:
+            x = self.dropout_layer(x)
+
+        x = self.lin_1(x)
+        if not self.use_logits:
+            x = torch.sigmoid(x)
+        return x
+
+    def init_weights(self):
+        self.apply(weights_init)
+
+if __name__ == "__main__":
+    D = Discriminator48(4, n_channels=3)
+    D(torch.rand((1, 3, 48, 48)))
---
+++ models/conv48/generator.py
@@ -0,0 +1,89 @@
+import torch
+from util.torch.activations import mish, LocalResponseNorm
+from util.torch.initialization import weights_init
+
+
+class Generator48(torch.nn.Module):
+    def __init__(self, latent_size, h_size, use_mish=False, n_channels=3, sigmoid_out=False, use_lr_norm=False):
+        super().__init__()
+
+        self.n_channels = n_channels
+
+        self.latent_size = latent_size
+        self.h_size = h_size
+        if use_mish:
+
+            self.activ = mish
+        else:
+            self.activ = self.leaky_relu
+        self.sigmoid_out = sigmoid_out
+
+        self.conv_1 = torch.nn.ConvTranspose2d(self.latent_size, self.h_size * 8, 4, bias=False)
+        self.conv_2 = torch.nn.ConvTranspose2d(self.h_size * 8, self.h_size * 4, kernel_size=7, stride=1, bias=False)
+        self.conv_3 = torch.nn.ConvTranspose2d(self.h_size * 4, self.h_size*4, kernel_size=4, stride=2, bias=False)
+        self.conv_4 = torch.nn.ConvTranspose2d(self.h_size * 4, self.h_size*2, kernel_size=5, stride=2, bias=False)
+        self.conv_5 = torch.nn.ConvTranspose2d(self.h_size*2, self.h_size*1, kernel_size=2, stride=1, bias=False)
+        self.conv_6 = torch.nn.Conv2d(self.h_size, self.n_channels, kernel_size=1, stride=1, bias=False)
+
+        self.output_bias = torch.nn.Parameter(torch.zeros((3, 48, 48)), requires_grad=True)
+
+        if not use_lr_norm:
+            self.bn_1 = torch.nn.BatchNorm2d(self.h_size * 8)
+            self.bn_2 = torch.nn.BatchNorm2d(self.h_size * 4)
+            self.bn_3 = torch.nn.BatchNorm2d(self.h_size * 4)
+            self.bn_4 = torch.nn.BatchNorm2d(self.h_size * 2)
+            self.bn_5 = torch.nn.BatchNorm2d(self.h_size)
+        else:
+            self.bn_1 = LocalResponseNorm()
+            self.bn_2 = LocalResponseNorm()
+            self.bn_3 = LocalResponseNorm()
+            self.bn_4 = LocalResponseNorm()
+            self.bn_5 = LocalResponseNorm()
+
+
+
+    @staticmethod
+    def leaky_relu(x):
+        return torch.nn.functional.leaky_relu(x, 0.02)
+
+    def forward(self, inp):
+        x = inp.view(-1, self.latent_size, 1, 1)
+
+        x = self.conv_1(x)
+        x = self.bn_1(x)
+        x = self.activ(x)
+
+        x = self.conv_2(x)
+        x = self.bn_2(x)
+        x = self.activ(x)
+
+        x = self.conv_3(x)
+        x = self.bn_3(x)
+        x = self.activ(x)
+
+        x = self.conv_4(x)
+        x = self.bn_4(x)
+        x = self.activ(x)
+
+        x = self.conv_5(x)
+        x = self.bn_5(x)
+        x = self.activ(x)
+
+        x = self.conv_6(x)
+        # TODO: This check allows for older models to run without the output bias. It will be removed in the future
+        if hasattr(self, "output_bias"):
+            x = x + self.output_bias
+        if self.sigmoid_out:
+            x = torch.sigmoid(x)
+        else:
+            x = torch.tanh(x)
+        return x
+
+    def init_weights(self):
+        self.apply(weights_init)
+
+
+if __name__ == "__main__":
+    G = Generator48(256, 64)
+    print(G(torch.normal(0, 1, (1, 256))).size())
+
