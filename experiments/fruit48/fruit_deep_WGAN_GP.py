e4cc08823107486e9df1136eecd58945b014562c
gerryflap <gerry2211@gmail.com>
2020-02-14 18:28:04 +0100

    Added StyleGAN like generator

---
+++ experiments/fruit48/fruit_deep_WGAN_GP.py
@@ -0,0 +1,68 @@
+from data.FruitDataset import FruitDataset
+from models.conv48.discriminator import Discriminator48
+from models.conv48.generator import Generator48
+from models.stylegan2.stylegan2_like_generator import DeepGenerator
+from trainloops.wgangp_train_loop import GanTrainLoop
+import util.output
+from torchvision import transforms
+import torch
+import argparse
+
+# Parse commandline arguments
+from trainloops.listeners.gan_image_sample_logger import GanImageSampleLogger
+from trainloops.listeners.loss_reporter import LossReporter
+from trainloops.listeners.model_saver import ModelSaver
+
+parser = argparse.ArgumentParser(description="Fruit WGAN-GP experiment.")
+parser.add_argument("--batch_size", action="store", type=int, default=64, help="Changes the batch size, default is 64")
+parser.add_argument("--lr", action="store", type=float, default=0.0001,
+                    help="Changes the learning rate, default is 0.0001")
+parser.add_argument("--h_size", action="store", type=int, default=64,
+                    help="Sets the h_size, which changes the size of the network")
+parser.add_argument("--epochs", action="store", type=int, default=100, help="Sets the number of training epochs")
+parser.add_argument("--d_steps", action="store", type=int, default=5,
+                    help="Amount of discriminator steps per generator step")
+parser.add_argument("--l_size", action="store", type=int, default=32, help="Size of the latent space")
+parser.add_argument("--cuda", action="store_true", default=False,
+                    help="Enables CUDA support. The script will fail if cuda is not available")
+parser.add_argument("--use_mish", action="store_true", default=False,
+                    help="Changes all activations except the ouput of D and G to mish, which might work better")
+parser.add_argument("--dropout_rate", action="store", default=0.0, type=float,
+                    help="Sets the dropout rate in D")
+parser.add_argument("--use_lr_norm", action="store_true", default=False,
+                    help="Uses local response norm, which will make the generator samples "
+                         "independent from the rest of the batch.")
+parser.add_argument("--lambd", action="store", type=float, default=10.0,
+                    help="Lambda, multiplier for gradient penalty")
+
+args = parser.parse_args()
+
+output_path = util.output.init_experiment_output_dir("fruit48", "deep_wgan_gp", args)
+
+dataset = FruitDataset("data/fruit/48x48/oranges/", transform=transforms.Compose([
+    transforms.ToTensor(),
+]), only_original=False)
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+# G = Generator48(args.l_size, args.h_size, args.use_mish, n_channels=3, sigmoid_out=True, use_lr_norm=args.use_lr_norm)
+G = DeepGenerator(args.l_size, args.h_size, 6, 3)
+D = Discriminator48(args.h_size, use_bn=False, use_mish=args.use_mish, n_channels=3, dropout=args.dropout_rate, use_logits=True)
+G_optimizer = torch.optim.Adam(G.parameters(), lr=args.lr, betas=(0.0, 0.9))
+D_optimizer = torch.optim.Adam(D.parameters(), lr=args.lr, betas=(0.0, 0.9))
+
+if args.cuda:
+    G = G.cuda()
+    D = D.cuda()
+
+D.init_weights()
+
+listeners = [
+    LossReporter(),
+    GanImageSampleLogger(output_path, args, pad_value=1),
+    ModelSaver(output_path, n=5, overwrite=True, print_output=True)
+]
+train_loop = GanTrainLoop(listeners, G, D, G_optimizer, D_optimizer, dataloader, D_steps_per_G_step=args.d_steps,
+                          cuda=args.cuda, epochs=args.epochs, lambd=args.lambd)
+
+train_loop.train()
---
+++ models/stylegan2/stylegan2_like_generator.py
@@ -0,0 +1,68 @@
+import torch
+
+
+class UpscaleLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, n_channels=3):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.to_rgb = torch.nn.Conv2d(out_channels, n_channels, kernel_size=1)
+
+    def forward(self, inp):
+        x = torch.nn.functional.upsample_bilinear(inp, scale_factor=2)
+        x = self.conv1(x)
+        x = torch.nn.functional.leaky_relu(x, 0.02)
+
+        x = self.conv2(x)
+        x = torch.nn.functional.leaky_relu(x, 0.02)
+
+        rgb = self.to_rgb(x)
+        return x, rgb
+
+
+class DeepGenerator(torch.nn.Module):
+    def __init__(self, latent_size, h_size, start_resolution, n_upscales, n_channels=3):
+        super().__init__()
+        self.start_resolution = start_resolution
+        self.n_upscales = n_upscales
+        self.h_size = h_size
+        self.n_channels = n_channels
+
+        self.lin = torch.nn.Linear(
+            latent_size,
+            int(h_size*(2**(n_upscales - 1)) * start_resolution**2)
+        )
+        self.first_rgb = torch.nn.Conv2d(h_size* 2 ** (n_upscales - 1), n_channels, kernel_size=1)
+
+        self.upscale_layers = []
+        for i in range(n_upscales):
+            layer = UpscaleLayer(
+                int(h_size * (2**(n_upscales - i - 1))),
+                int(h_size * (2**(n_upscales - i - 2))),
+                n_channels
+            )
+            self.upscale_layers.append(layer)
+
+    def forward(self, z):
+        x = self.lin(z)
+        x = torch.nn.functional.leaky_relu(x, 0.02)
+        x = x.view(
+            -1,
+            int(self.h_size * (2**(self.n_upscales - 1))),
+            self.start_resolution,
+            self.start_resolution
+        )
+
+        rgb_out = self.first_rgb(x)
+
+        for layer in self.upscale_layers:
+            x, rgb = layer(x)
+            rgb_out = torch.nn.functional.upsample_bilinear(rgb_out, scale_factor=2)
+            rgb_out += rgb
+
+        return torch.sigmoid(rgb_out)
+
+
+if __name__ == "__main__":
+    G = DeepGenerator(256, 4, 6, 3)
+    print(G(torch.normal(0, 1, (2, 256))).size())
