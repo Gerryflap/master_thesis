445e328c41c6960d0722d0682edbdd37d3006e7c
gerryflap <gerry2211@gmail.com>
2020-02-16 00:10:49 +0100

    Modified the z input method

--- models/stylegan2/stylegan2_like_alt_ali_discriminator.py
+++ models/stylegan2/stylegan2_like_alt_ali_discriminator.py
@@ -1,11 +1,44 @@
 import torch
 
 from util.torch.initialization import weights_init
-from models.stylegan2.stylegan2_like_discriminator import DownscaleLayer
+
+class DownscaleLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, z_channels, bn=False):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels + z_channels, out_channels, kernel_size=3, stride=2, padding=1)
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.lower_channels = torch.nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+        self.bn = bn
+        if bn:
+            self.bn_1 = torch.nn.BatchNorm2d(out_channels)
+            self.bn_2 = torch.nn.BatchNorm2d(out_channels)
+
+    def forward(self, inp):
+        inp, z_in = inp
+
+        carry = torch.nn.functional.interpolate(inp, scale_factor=0.5, mode='bilinear', align_corners=True)
+        carry = self.lower_channels(carry)
+
+        x = torch.cat([inp, z_in], dim=1)
+
+        x = self.conv1(x)
+        x = torch.nn.functional.leaky_relu(x, 0.02)
+
+        if self.bn:
+            x = self.bn_1(x)
+
+        x = self.conv2(x)
+        x = torch.nn.functional.leaky_relu(x, 0.02)
+
+        if self.bn:
+            x = self.bn_2(x)
+
+        return carry + x
 
 
 class DeepAltAliDiscriminator(torch.nn.Module):
-    def __init__(self, latent_size, h_size, input_resolution, n_downscales, n_channels=3, bn=False, z_channels=4):
+    def __init__(self, latent_size, h_size, input_resolution, n_downscales, n_channels=3, bn=False, z_channels=16):
         super().__init__()
         self.input_resolution = input_resolution
         self.n_downscales = n_downscales
@@ -49,8 +82,7 @@ class DeepAltAliDiscriminator(torch.nn.Module):
             z_channels = self.activ(z_layer(z))
             z_channels = z_channels.view(-1, self.z_channels, 1, 1)
             z_channels = z_channels.repeat(1, 1, x.size(2), x.size(3))
-            x = torch.cat([z_channels, x], dim=1)
-            x = layer(x)
+            x = layer((x, z_channels))
 
         x = x.view(-1, int(self.h_size * (2 ** self.n_downscales)) * self.conv_out_res * self.conv_out_res)
         x = self.x_fc(x)
