2d3d692d9c6cde67a7829b2be31d9c90bba39b3c
gerryflap <gerry2211@gmail.com>
2020-02-15 12:19:32 +0100

    Added an attempt at normalized learning rates

--- util/torch/modules.py
+++ util/torch/modules.py
@@ -7,3 +7,25 @@ class Reshape(torch.nn.Module):
 
     def forward(self, x):
         return x.view(self.shape)
+
+
+# Untested and unused
+class Conv2dNormalizedLR(torch.nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size=1, stride=1, padding=0, bias=True):
+        super().__init__()
+        self.stride = stride
+        self.padding = padding
+        self.he_constant = torch.sqrt(2.0/float(in_channels))
+
+        self.weight = torch.nn.Parameter(torch.Tensor(in_channels, out_channels, kernel_size, kernel_size))
+
+        if bias:
+            self.bias = torch.nn.Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter("bias", None)
+        self.reset_parameters()
+
+    def forward(self, inp):
+        weight = self.weight/self.he_constant
+        x = torch.nn.functional.conv2d(inp, weight, self.bias, self.stride, self.padding)
+        return x
