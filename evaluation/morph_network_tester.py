5475141853eb6ba636e2fcee923770f6687e66cf
gerben <gerry2211@gmail.com>
2020-04-16 10:21:41 +0200

    Added an option to put all lists in the same order as the sorted z_morph list

--- evaluation/morph_network_tester.py
+++ evaluation/morph_network_tester.py
@@ -7,6 +7,8 @@ from util.interpolation import torch_slerp
 parser = argparse.ArgumentParser(description="Image to latent vector converter.")
 parser.add_argument("--enc", required=True, action="store", type=str, help="Path to Gz/Encoder model")
 parser.add_argument("--n_samples", action="store", type=str, help="Number of z morphs to generate", default=1000)
+parser.add_argument("--use_z_morph_sort", action="store_true", help="Sorts z1 and puts all others in that order as well")
+
 args = parser.parse_args()
 
 Gz = torch.load(args.enc).cpu()
@@ -20,18 +22,26 @@ z_morph = Gz.morph_zs(z1, z2)
 linear_morph = 0.5*(z1 + z2)
 slerp_morph = torch_slerp(0.5, z1, z2)
 
-z1_l2s = z1.norm(2, 1).detach().sort()[0]
-z2_l2s = z2.norm(2, 1).detach().sort()[0]
-z_morph_l2s = z_morph.norm(2, 1).detach().sort()[0]
-linear_morph_l2s = linear_morph.norm(2, 1).detach().sort()[0]
-dist_z_morph_to_linear = (linear_morph - z_morph).norm(2, 1).detach().sort()[0]
-slerp_morph_l2s = slerp_morph.norm(2, 1).detach().sort()[0]
+z_morph_l2s, z_sort = z_morph.norm(2, 1).detach().sort()
+if args.use_z_morph_sort:
+    z1_l2s = z1.norm(2, 1)[z_sort].detach()
+    z2_l2s = z2.norm(2, 1)[z_sort].detach()
+    linear_morph_l2s = linear_morph.norm(2, 1)[z_sort].detach()
+    dist_z_morph_to_linear = (linear_morph - z_morph)[z_sort].norm(2, 1).detach()
+    slerp_morph_l2s = slerp_morph.norm(2, 1)[z_sort].detach()
+else:
+    z1_l2s = z1.norm(2, 1).detach().sort()[0]
+    z2_l2s = z2.norm(2, 1).detach().sort()[0]
+    z_morph_l2s = z_morph.norm(2, 1).detach().sort()[0]
+    linear_morph_l2s = linear_morph.norm(2, 1).detach().sort()[0]
+    dist_z_morph_to_linear = (linear_morph - z_morph).norm(2, 1).detach().sort()[0]
+    slerp_morph_l2s = slerp_morph.norm(2, 1).detach().sort()[0]
 
 
 plt.plot(z1_l2s.numpy(), label="z1")
 plt.plot(z2_l2s.numpy(), label="z2")
 plt.plot(z_morph_l2s.numpy(), label="z_morph")
-# plt.plot(linear_morph_l2s.numpy(), label="linear morph")
+plt.plot(linear_morph_l2s.numpy(), label="linear morph")
 # plt.plot(slerp_morph_l2s.numpy(), label="slerp morph")
 # plt.plot(dist_z_morph_to_linear, label="L2 dist between linear and z_morph")
 plt.title("Sorted $L_2$ norms of latent vector distributions")
